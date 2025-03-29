db0c6035ffb01dff2af6cc8a79ab274ffaf0e5c6
olaoluwajo <olagundoyealexander@gmail.com>
2025-03-29 16:53:20 +0100

    Implement FAQ section with collapsible questions and answers; replace CTA section with FAQComponent

--- src/app/(external)/contact/page.tsx
+++ src/app/(external)/contact/page.tsx
@@ -13,7 +13,6 @@ const Page = () => {
 					icon={clock}
 					iconSubheading="Available 24/7 for emergency road service"
 				/>
-				{/* <ContactInfo/> */}
 				<ContactInfo
 					whatsappNumber="+234 809 653 5511"
 					email="contact@obana.africa"
@@ -27,15 +26,3 @@ const Page = () => {
 };
 
 export default Page;
-
-{
-	/* <iframe
-	src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d15853.89503380595!2d3.352339207490718!3d6.587882111058755!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x103b926a7242ffff%3A0xe642357dcd25af85!2sICON%20WHOLESALE%20-%20BUY%20FASHION%20WHOLESALE!5e0!3m2!1sen!2sng!4v1743088549210!5m2!1sen!2sng"
-	width="600"
-	height="450"
-	style="border:0;"
-	allowfullscreen=""
-	loading="lazy"
-	referrerpolicy="no-referrer-when-downgrade"
-></iframe>; */
-}
