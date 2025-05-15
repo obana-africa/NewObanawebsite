9876c0fe9357b56d22db98c30b5efbc8624fb66c
KOREDE BUSUYI <89644512+Korexszman@users.noreply.github.com>
2025-05-15 13:05:08 +0100

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,8 +6,8 @@ import "slick-carousel/slick/slick.css";
 import "react-phone-input-2/lib/style.css";
 
 export const metadata: Metadata = {
-	title: "Obana - Enabling SMEs Scale",
-	description: "Obana | Sub-Sahara Africa's Sourcing Marketplace Platform.",
+	title: "Sub-Saharan Africa's Leading Marketplace for Fashion & Beauty",
+	description: "Sub-Saharan Africa's Leading Marketplace for Fashion & Beauty",
 	icons: {
 		icon: "/favicon.png?v=1",
 	},
