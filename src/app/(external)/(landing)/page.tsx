78347c1637772fbf0c681b7e8e924475a43e0841
olaoluwajo <olagundoyealexander@gmail.com>
2025-09-01 15:38:04 +0100

    Refactor BNPL component layout and update package.json scripts

--- src/app/(external)/(landing)/page.tsx
+++ src/app/(external)/(landing)/page.tsx
@@ -10,15 +10,14 @@ import WhyObanaSection from "./sections/why-obana";
 import CtaSection from "../../../components/external/components/cta-section";
 import Testimonials from "./sections/testimonials-section";
 import ImpactCarousel from "./sections/impact-section";
-import BNPL from "./sections/BNPL"; // ✅ import the first component
-// 
+import BNPL from "./sections/BNPL"; 
 
 const Home: NextPage = () => {
 	return (
 		<div className=" min-h-screen">
 			<main>
 				<HeroSection />
-                <BNPL />
+				<BNPL />
 				<GlobalBrands />
 				<ServiceSection features={serviceFeatures} imageSrc={shipbox} />
 				<StakeholderSection
