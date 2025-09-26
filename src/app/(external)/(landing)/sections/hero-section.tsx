041defe73a870e5eb417ff3816468acebf78b062
obana-africa <obana.africa@gmail.com>
2025-09-26 14:01:28 +0100

    Update hero-section.tsx22

--- src/app/(external)/(landing)/sections/hero-section.tsx
+++ src/app/(external)/(landing)/sections/hero-section.tsx
@@ -63,7 +63,7 @@ const HeroSection: React.FC = () => {
 						</h3>
 
 						<h2 className="mb-6 md:leading-12 w-full 2xl:w-[85%]">
-							Aggregating sourcing, logistics, and financing into one seamless platform
+							Digital Ecosystem Powering Africa's Circular Fashion Economy
 						</h2>
 
 						<p className="mb-8 text-md 2xl:text-lg ">
