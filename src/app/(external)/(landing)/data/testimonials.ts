8b041b58046d72e7008a64f4823a37ff31087951
olaoluwajo <olagundoyealexander@gmail.com>
2025-11-12 01:44:13 +0100

    ongoing new design

--- src/app/(external)/(landing)/data/testimonials.ts
+++ src/app/(external)/(landing)/data/testimonials.ts
@@ -1,33 +1,38 @@
 import mitchel from "@/app/assets/images/landing-page/mitchel.jpg";
 import adenike from "@/app/assets/images/landing-page/adenike.jpg";
+import vendor from "@/app/assets/images/landing-page/vendor.png";
 
 export const testimonials = [
 	{
+		quote:
+			"I was able help entrepreneurs access quality products hassle-free while building my own success on the Platform",
+		rating: "10/10 recommended",
 		name: "Mitchel Ahisu",
-		role: " Sales Partner  ",
-		message:
-			'"I was able help entrepreneurs access quality products hassle-free while building my own success on the Platform"',
+		role: "Sales Partner",
 		image: mitchel,
 	},
 	{
+		quote:
+			"Being an Obana.africa Sales Partner has empowered me to earn, grow, and connect businesses to limitless sourcing opportunities.",
+		rating: "10/10 recommended",
 		name: "Adenike Folorunsho",
 		role: "Sales Partner",
-		message:
-			'"Being an Obana.africa Sales Partner has empowered me to earn, grow, and connect businesses to limitless sourcing opportunities."',
-		image: adenike,
+		image: vendor,
 	},
 	{
+		quote:
+			"I was able help entrepreneurs access quality products hassle-free while building my own success on the Platform",
+		rating: "10/10 recommended",
 		name: "Mitchel Ahisu",
-		role: " Sales Partner  ",
-		message:
-			'"I was able help entrepreneurs access quality products hassle-free while building my own success on the Platform"',
+		role: "Sales Partner",
 		image: mitchel,
 	},
 	{
+		quote:
+			"Being an Obana.africa Sales Partner has empowered me to earn, grow, and connect businesses to limitless sourcing opportunities.",
+		rating: "10/10 recommended",
 		name: "Adenike Folorunsho",
 		role: "Sales Partner",
-		message:
-			'"Being an Obana.africa Sales Partner has empowered me to earn, grow, and connect businesses to limitless sourcing opportunities."',
 		image: adenike,
 	},
 ];
