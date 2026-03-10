74f89648cb790057c9167402ebaeba33436efa32
obana-africa <obana.africa@gmail.com>
2026-03-10 10:02:47 +0100

    Update page.tsx

--- src/app/(external)/rfq/page.tsx
+++ src/app/(external)/rfq/page.tsx
@@ -1,23 +1,5 @@
-"use client";
+import SinglePageRFQ from "./single-page";
 
-import Breadcrumb from "@/components/external/components/breadcrumb";
-import React from "react";
-import HowRfqWorks from "./components/how-rfq-works";
-import ProductionOnlyForm from "./components/production-only-form";
-
-const Page = () => {
-	return (
-		<div className="min-h-screen overflow-x-hidden" id="rfq-page">
-			<main>
-				<Breadcrumb
-					heading="RFQ (Request for quote)"
-					subheading="Get accurate pricing tailored to your production needs with our seamless quote request process for Fashion & Beauty SMEs."
-				/>
-				<HowRfqWorks />
-				<ProductionOnlyForm />
-			</main>
-		</div>
-	);
-};
-
-export default Page;
+export default function Page() {
+  return <SinglePageRFQ />;
+}
